872358a09904aa1b308fb52bacee883ffb55b656
JustMeKacper <kmredis@gmail.com>
2023-04-07 18:47:16 +0200

    Dodaj pliki projektów.

---
+++ Server/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using BlazorEcommerce.Server.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private static List<Product> Products = new List<Product>()
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "Galaxy",
+                Description = "Who cares about Galaxy",
+                ImageUrl = "https://images.photowall.com/products/71364/milky-way-galaxy.jpg?h=699&q=85",
+                Price = 9.99m
+            },new Product
+            {
+                Id = 2,
+                Title = "Mayo",
+                Description = "Mjód na moje serce",
+                ImageUrl = "https://www.winiary.pl/sites/default/files/styles/product_image_desktop_617_900/public/Winiary%20Majonez%20dekoracyjny%20400ml%203d_0.png?itok=dm9MOq3L",
+                Price = 7.99m
+            },new Product
+            {
+                Id = 3,
+                Title = "Rykoszet",
+                Description = "Najlepszy portal z newsami o world of tanks",
+                ImageUrl = "https://rykoszet.info/wp-content/uploads/2022/02/MYhGBv2.png",
+                Price = 1113.99m
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
---
+++ Server/Migrations/20230407164259_ProductSeeding2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class ProductSeeding2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "Who cares about Galaxy", "https://images.photowall.com/products/71364/milky-way-galaxy.jpg?h=699&q=85", 9.99m, "Galaxy" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Mjód na moje serce", "https://www.winiary.pl/sites/default/files/styles/product_image_desktop_617_900/public/Winiary%20Majonez%20dekoracyjny%20400ml%203d_0.png?itok=dm9MOq3L", 7.99m, "Mayo" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Najlepszy portal z newsami o world of tanks", "https://rykoszet.info/wp-content/uploads/2022/02/MYhGBv2.png", 1113.99m, "Rykoszet" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
